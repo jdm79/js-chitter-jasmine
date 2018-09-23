36888a7f7c67f37651a10545d0da3a422750e09a
jdm79 <james.malvern@gmail.com>
2018-09-23 14:25:24 +0100

    display all peeps on the index.html

--- src/Chitter.js
+++ src/Chitter.js
@@ -1,12 +1,16 @@
 (function(exports) {
+
+  const allPeepsUrl = 'https://chitter-backend-api.herokuapp.com/peeps'
+
   function Chitter() {
     this.peeps = []
   }
-  chitter = new Chitter()
+  this.chitter = new Chitter()
+ 
 
   
-  Chitter.prototype.getPeeps = function() {
-    fetch('https://chitter-backend-api.herokuapp.com/peeps')
+  Chitter.prototype.getPeeps = function(allPeepsUrl) {
+    fetch()
       .then(res => {
         if (res.ok) {
           return res.json()
@@ -18,25 +22,51 @@
         }
       })
       .then(data => {
-        console.log(data)
         this.peeps = data
+        console.log(this.peeps)
+        console.log(data)
+
         return this.peeps
       })
+      
       .catch(error => {
         if (error.status === 404) {
           console.log('error is ', error.status, error)
         }
       })
     }
+    
   
-  Chitter.prototype.showPeeps = function() {
-    Chitter.getPeeps()
-    console.log("this is the showPeeps function", this.peeps)
+  Chitter.prototype.showPeeps = function() {    
+    function createNode(element) {
+      return document.createElement(element)
+      }
+
+      function append(parent, el) {
+        return parent.appendChild(el)
+      }
+
+      const ul = document.getElementById('list-items')
+      fetch(allPeepsUrl)
+      .then((res) => res.json())
+      .then(function(data) {
+        let peeps = data
+        return peeps.map(function(peep) {
+          console.log(peep.body)
+          let li = createNode('li')
+          li.innerHTML = `a peep ${peep.body}`
+          append(ul, li)
+        })
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
     }
   
   Chitter.prototype.showIndividualPeep = function() {
-    document.getElementById('test').innerHTML = 
-        `<li>${data[0].body}<p>by ${data[0].id}</p></li>
-        `
     }    
+
+  chitter.getPeeps()
+  chitter.showPeeps()
+
 })(this)
